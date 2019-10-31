4d6a54584c4adf5062f29437ab17c04b27fcbff4
cmorosini <55539208+cmorosini@users.noreply.github.com>
2019-10-31 21:30:38 +1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Enter how many minutes you want to convert to hours: ");
+            int minutes = int.Parse(Console.ReadLine());
+            int hours = minutes / 60;
+            int mins = minutes % 60;
+
+            Console.WriteLine("That is {0} hours and {1} minutes", hours, mins);
+            Console.WriteLine("Press any key to exit..");
+            Console.ReadKey();
+        }
+    }
+}
